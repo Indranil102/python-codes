d1a29afe343cc6f68dc79b9869c5351c0ba965a9
Indranil Samanta <115712973+Indranil102@users.noreply.github.com>
2024-04-18 23:29:58 +0530

    Add files via upload

---
+++ getting_input_typecasting.py
@@ -0,0 +1,9 @@
+a=int(input("enter number "))
+b=int(input("enter another number "))
+#print(int(a)+int(b))
+print(a+b)
+
+#eval can convert any data type
+x=eval(input("enter number "))
+y=eval(input("enter number "))
+print(x*y)
