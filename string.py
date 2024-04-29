c05e2c25b59c77eb5298c7d7fb95af5302ede151
Indranil Samanta <115712973+Indranil102@users.noreply.github.com>
2024-04-29 21:59:26 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,14 @@
+# it is multiple character
+# space also has index
+w="i am here"
+print(type(w))
+print(w[6])
+print(w[-2])# string indexing
+print(w[-6])
+# string slicing
+
+
+print(w[0:3])
+print(w[0::2])# read all character  and increment by 2
+print(w[-1:-10:-2])
+print(w[-1::-1])
